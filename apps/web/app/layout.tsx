90b870401299d33ff7d90450c86efdd9fed54d79
Sputi93 <federico.putignano93@gmail.com>
2026-03-21 18:01:02 +0100

    layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
